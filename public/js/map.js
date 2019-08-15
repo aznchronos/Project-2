483637f58d0231071cae799edbbf10ba70aa3c33
Geoff <gardgeoff95@email.arizona.edu>
2019-08-14 17:05:12 -0700

    map movements

---
+++ public/js/map.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    console.log("jquery ready");
+
+    var character = $("#character");
+
+    $("#up").on("click", function() {
+        
+
+        $(character).animate({top: "570px"}, 2000);
+    });
+});
