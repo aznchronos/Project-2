0e12238a5bbf8850b79234cac698f9df3e70f097
Geoff <gardgeoff95@email.arizona.edu>
2019-08-15 16:20:13 -0700

    map structure

--- public/js/character.js
+++ public/js/character.js
@@ -1,13 +1,77 @@
-$(document).ready(function() {
-  console.log("jquery loaded");
-  go = $("#go");
+function openDoor() {
+  $("#door").attr("src", "images/dooropen.png");
+  type = 1; 
+  cycleLoop = [1, 2, 3, 4, 5, 6];
+  $("canvas").animate({ marginLeft: "850px" }, 2000, function() {
+      $("body").fadeOut(3500);
 
-  $(go).on("click", function() {
-    $("canvas").animate({ marginLeft: "700px" }, 2000, function() {
-      $("#door").attr("src", "images/dooropen.png");
-      $("canvas").animate({ marginLeft: "850px" }, 2000, function() {
-        $("body").fadeOut(6000);
-      });
     });
+}
+$("#go").on("click", function() {
+  cycleLoop = [1,2,3,4,5,6];
+  type = 1;
+  $("canvas").animate({ marginLeft: "720px" }, 2000, function() {
+    cycleLoop = [0, 1, 2, 3];
+    type =0;
+    setTimeout(openDoor, 1000);
+  
+    
+  
+
+    
   });
-});
+})
+
+var idleFrames = [0, 1, 2, 3];
+var type = 0;
+
+let img = new Image();
+img.src = '../images/AdSprite.png';
+
+
+img.onload = function () {
+  init();
+
+}
+
+let canvas = document.getElementById("characterAnimation");
+let ctx = canvas.getContext('2d');
+
+const scale = 5;
+const width = 50;
+const height = 37;
+const scaledWidth = width * scale;
+const scaledHeight = height * scale;
+
+function init() {
+  window.requestAnimationFrame(step);
+}
+
+let cycleLoop = [0,1,2,3];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+
+function step() {
+
+  frameCount++;
+  if (frameCount < 6) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFrame(cycleLoop[currentLoopIndex], type, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+    frameX * width, frameY * height, width, height,
+    canvasX, canvasY, scaledWidth, scaledHeight);
+}
--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,79 @@
 $(document).ready(function() {
-  console.log("jquery ready");
-
+  //character element from html
   var character = $("#character");
 
-  $("#up").on("click", function() {
-    $(character).animate({ top: "570px" }, 2000);
-  });
+  //object of possible encoutners within the map called later in the position functions
+  var possibleEncounter = {
+    getItem: function() {
+      console.log("You got an Item!");
+    },
+    statBoost: function() {
+      console.log("You got a stat boost!");
+    },
+    enounter: function() {
+      console.log("Enemy encounter!");
+    }
+  };
+
+  //function that alters buttons based on where you are on the map
+  function alterButton(button, swap, action) {
+    $("#" + button).css("visibility", swap);
+    $("#" + button).html(action);
+  }
+  
+  var position = 1; // counter variable that keeps track of where you are on the map
+  //switch case called on page load then recursively in each position function
+  function switcher() {
+    switch (position) {
+    case 1:
+      position1();
+      break;
+
+    case 2:
+      position2();
+    case 3:
+      position3();
+    }
+  }
+  function position1() {
+    //each of these functions swap out the controls based on your current position
+    alterButton("left", "hidden", "left");
+    alterButton("right", "hidden", "right");
+    alterButton("down", "hidden", "down");
+    $("#up").on("click", function() {
+      $(character).animate({ top: "600px" }, 2000, function() {
+        position = 2;
+        switcher();
+      });
+    });
+  }
+  function position2() {
+    alterButton("left", "visible", "left (inspect)");
+    $("#left").on("click", function() {
+      console.log("hello!");
+      $(character).animate({ left: "20px" }, 2000, function() {
+        possibleEncounter.enounter();
+      });
+    });
+    $("up").on("click", function() {
+      alterButton("left", "hidden", "left");
+      $(character).animate({ top: "500" }, 2000, function() {
+        position = 3;
+        switcher();
+      });
+    });
+  }
+
+  function position3() {
+    alterButton("right", "visible", "right");
+    $("up").on("click", function() {
+      $(character).animate({ top: "300" }, 2000, function() {
+        position = 4;
+        switcher();
+      });
+    });
+  };
+
+
+  switcher();
 });
